e38dedcf95e715ac1e999e4f46c4240f1c7300c4
sofiane-benmahiddine <sof.benmahiddine@gmail.com>
2022-02-05 14:41:29 +0100

    test api

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 import express from "express";
 
 const app = express();
-
+app.use(express.json());
 app.listen("8080");
 
-app.get("/", (req, resp) => {
-  resp.send("Hello World!");
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/signin", (req, res) => {
+  console.log(req.body)
+  res.send("signin");
 });
